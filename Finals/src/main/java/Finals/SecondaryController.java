826bb80eb1f5e0929263a936ac9f97294da36f73
Rubber_Goat <z5283527@ad.unsw.edu.au>
2021-04-22 15:25:59 +1000

    Create SecondaryController.java

---
+++ Finals/src/main/java/Finals/SecondaryController.java
@@ -0,0 +1,12 @@
+package Finals;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
