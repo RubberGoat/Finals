15048178c747ca00d4763241c13a5c57280ad192
Rubber_Goat <z5283527@ad.unsw.edu.au>
2021-04-22 23:31:43 +1000

    Update ThreadHelper.java

--- Finals/src/main/java/Finals/ThreadHelper.java
+++ Finals/src/main/java/Finals/ThreadHelper.java
@@ -5,10 +5,45 @@
  */
 package Finals;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  *
  * @author aldo_
  */
 public class ThreadHelper {
+    
+    
+    public static void writeToFile(String filename, String content) {
+        try (FileWriter fw = new FileWriter(filename, true);
+             BufferedWriter bw = new BufferedWriter(fw);
+             PrintWriter out = new PrintWriter(bw)) {
+            out.println(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int countLines(String filename) {
+        int lines = 0;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            while (reader.readLine() != null) {
+                lines++;
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            return -1;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
     
 }
