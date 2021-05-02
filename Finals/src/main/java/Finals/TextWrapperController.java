565e448926bec176a439fdaae2fc2009d96189f6
Rubber_Goat <z5283527@ad.unsw.edu.au>
2021-05-03 00:44:10 +1000

    Create TextWrapperController.java

---
+++ Finals/src/main/java/Finals/TextWrapperController.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Finals;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Aldo
+ */
+public class TextWrapperController {
+    public void initialize() throws FileNotFoundException, IOException, SQLException{     
+      //get pdf from database
+      
+      //show pdf into list view
+      
+    }
+    
+    public void NewPdf() throws FileNotFoundException, IOException, SQLException{     
+        //open file chooser
+        
+        //
+        
+      
+    }
+    
+    public void WrapPdf() throws FileNotFoundException, IOException, SQLException{     
+        //
+        
+        //
+        
+      
+    }
+    
+    
+}
