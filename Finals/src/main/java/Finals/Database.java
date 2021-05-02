7d817dea4fd017d0e7a3e999194465f4c801175c
Rubber_Goat <z5283527@ad.unsw.edu.au>
2021-05-03 03:42:22 +1000

    Update Database.java

--- Finals/src/main/java/Finals/Database.java
+++ Finals/src/main/java/Finals/Database.java
@@ -5,10 +5,14 @@
  */
 package Finals;
 
+import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 /**
  *
@@ -30,18 +34,16 @@ public class Database {
 
             // Create  Table, with id, name, species, colour, and owner fields
             String createStuff= "CREATE TABLE IF NOT EXISTS Stuff ("
-                    + "stuff_id NUMBER (255) NOT NULL autoincrement"
-                    + ", stuff_name VARCHAR (255) NOT NULL"
-                    + ", PRIMARY KEY (stuff_id))";
+                    + "stuff_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT"
+                    + ", stuff_name VARCHAR (255) NOT NULL)";
             
              // Close connections and statements
             st.execute(createStuff);
             
             String createAttachment = "CREATE TABLE IF NOT EXISTS Attachment ("
-                    + "attachment_id NUMBER (255) NOT NULL autoincrement"
+                    + "attachment_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT"
                     + ", attachment_name VARCHAR (255) NOT NULL"
-                    + ", attachment_data LONGBLOB NOT NULL"
-                    + ", PRIMARY KEY (attachment_id))";
+                    + ", attachment_data LONGBLOB NOT NULL)";
             
              // Close connections and statements
             st.execute(createAttachment);
@@ -49,7 +51,55 @@ public class Database {
         
         //insert data
         
-        //get database
+        //print attachment
+         public static void printAll() throws SQLException{
+            Connection conn = DriverManager.getConnection("JDBC:sqlite:FinalsDatabase.db");
+
+            //create statement
+            Statement st = conn.createStatement ();
+
+            
+            System.out.println("AttachmentList");
+
+            
+            //print attachment
+
+            String selectAT = "SELECT * FROM Attachment ";
+
+            ResultSet sc = st.executeQuery(selectAT);
+
+            while(sc.next()){
+                    System.out.println(sc.getString(1) + ", " 
+                    + sc.getString(2) + ", " 
+                    + sc.getString(3));
+            }
+             
+         }
+        
+        //get attachment
+        public static ObservableList<Attachment> getAtch() throws SQLException {
+        // Get ResultSet
+        Connection conn = DriverManager.getConnection("jdbc:sqlite:FinalsDatabase.db");
+        Statement st = conn.createStatement();
+        
+        String query = "SELECT attachment_id, attachment_name, attachment_data FROM Attachment";
+        ResultSet rs = st.executeQuery(query);
+        
+        ObservableList<Attachment> AtchList = FXCollections.observableArrayList();
+        // Add each row in the ResultSet
+        
+        while(rs.next()){
+            AtchList.add(new Attachment(rs.getInt(1),
+                    rs.getString(2), (Blob) rs.getBinaryStream(3)));
+        }     
+        
+
+  
+        // Close 
+        st.close();
+        conn.close();
+        return AtchList;
+    }
         
         //login boolea
         
