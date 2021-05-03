08debd03796f7a932b59e3a4050941f3bbed8dae
Rubber_Goat <z5283527@ad.unsw.edu.au>
2021-05-03 23:06:32 +1000

    Update HomePageController.java

--- Finals/src/main/java/Finals/HomePageController.java
+++ Finals/src/main/java/Finals/HomePageController.java
@@ -1,21 +1,17 @@
 package Finals;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import javafx.fxml.FXML;
 
 public class HomePageController {
 
-    
-    MessageLoop threadOne;
-    MessageLoop threadTwo;
-    MessageLoop threadThree;
+   
     
     //initialize
     @FXML
     public void initialize() {
-        MessageLoop threadOne = new MessageLoop("class1.txt","I will behave",10);
-        MessageLoop threadTwo = new MessageLoop("class2.txt","I will finish my homework",15);
-        MessageLoop threadThree = new MessageLoop("class3.txt","I will listen to my teacher",20);
+        System.out.println("Local Date " + LocalDate.now());
     }
     
     @FXML
