800b5850b50f8a019586afa54e7cb42fb778ff0d
Rubber_Goat <z5283527@ad.unsw.edu.au>
2021-05-06 11:51:00 +1000

    Create ThreadHelper.java

---
+++ MemeGenerator/src/main/java/Finals/ThreadHelper.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Finals;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author aldo_
+ */
+public class ThreadHelper {
+    
+    
+    public static void writeToFile(String filename, String content) {
+        try (FileWriter fw = new FileWriter(filename, true);
+             BufferedWriter bw = new BufferedWriter(fw);
+             PrintWriter out = new PrintWriter(bw)) {
+            out.println(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int countLines(String filename) {
+        int lines = 0;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            while (reader.readLine() != null) {
+                lines++;
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            return -1;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+    
+}
