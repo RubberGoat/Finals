076e01252e4de96563f509519e5937b36245369f
Rubber_Goat <z5283527@ad.unsw.edu.au>
2021-05-06 11:54:36 +1000

    Create MessageLoop.java

---
+++ MemeGenerator/src/main/java/MemeGenerator/MessageLoop.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MemeGenerator;
+
+/**
+ *
+ * @author Aldo
+ */
+public class MessageLoop extends Thread {
+    private String filename;
+    private String message;
+    private int numberTimes;
+
+    //Write a constructor for the MessageLoop class
+
+    public MessageLoop (String filename, String message, int numberTimes){
+        this.filename = filename;
+        this.message = message;
+        this.numberTimes = numberTimes;
+
+    }
+
+
+    //Write the method that is used to begin the thread - hint: what interface was implemented in this MessageLoop class
+    public void run(){
+        try{
+          
+
+            for(int i = 0; i < this.numberTimes ; i ++){
+                ThreadHelper.writeToFile(this.filename,this.message);
+                 Thread.sleep(20);
+            }
+
+        } catch (Exception e){
+            System.out.println("NEIIIIIIIIIIGH");
+        }
+    }
+
+
+    
+}
