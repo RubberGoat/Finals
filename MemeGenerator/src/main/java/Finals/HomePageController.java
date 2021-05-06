de4f6947a172caac93e9ad1904fc634aed99d28e
Rubber_Goat <z5283527@ad.unsw.edu.au>
2021-05-06 11:50:45 +1000

    Create HomePageController.java

---
+++ MemeGenerator/src/main/java/Finals/HomePageController.java
@@ -0,0 +1,29 @@
+package Finals;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import javafx.fxml.FXML;
+
+public class HomePageController {
+
+   
+    
+    //initialize
+    @FXML
+    public void initialize() {
+        System.out.println("Local Date " + LocalDateTime.now());
+    }
+    
+    @FXML
+    private void TextWrapper() throws IOException{
+                App.setRoot("TextWrapper");
+    }
+    
+    @FXML
+    private void MemeGenerator() throws IOException{
+                App.setRoot("MemeGenerator");
+    }
+   
+    
+}
